68c3fd92a32c138a9bb30c21f05253d9ec8ef98d
Abdulaziz Mohammed <azozmohd38@gmail.com>
2026-09-08 15:25:02 +0400

    Define the table base as fixed

--- MultiplicationTable/MultiplicationTable.java
+++ MultiplicationTable/MultiplicationTable.java
@@ -3,7 +3,7 @@ package PACKAGE_NAME;
 public class MultiplicationTable {
     public static void main(String[]args){
        //Declare an int variable named base and assign it a value of 9
-       int base = 9;
+       final int base = 9;
        //Use a for loop that runs a counter variable from 1 to 10
         for (int i = 1; i <= 10  ; i++){
          int result = base * i ;
