6f4efa25d8319d90226e9c4d95d75e336b2a7af7
Abdulaziz Mohammed <azozmohd38@gmail.com>
2026-09-08 15:13:05 +0400

    Set factorial

--- FactorialCalculator/FactorialCalculator.java
+++ FactorialCalculator/FactorialCalculator.java
@@ -1,5 +1,6 @@
 public class FactorialCalculator {
 
+
     public static void main(String[] arge){
         //Declare an int variable named number and assign it a value of 7\
              int number = 7;
