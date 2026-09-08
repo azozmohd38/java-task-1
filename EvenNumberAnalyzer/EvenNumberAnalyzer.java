d189752303002b4301bb85c6cf692627dd3fe8f4
Abdulaziz Mohammed <azozmohd38@gmail.com>
2026-09-08 15:24:25 +0400

    Define the limit as a fixed value

--- EvenNumberAnalyzer/EvenNumberAnalyzer.java
+++ EvenNumberAnalyzer/EvenNumberAnalyzer.java
@@ -9,7 +9,7 @@ public class EvenNumberAnalyzer {
 
     public static void main(String[] args) {
         //Declare an int variable named limit and set it to 20
-        int limit = 20;
+        final int limit = 20;
         //Declare an int variable named count and initialise it to 0
         int count = 0;
         //Declare an int variable named sum and initialise it to 0
