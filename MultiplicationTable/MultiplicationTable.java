5cda533bfa5a9cb295e6c2803496cd4dcf4c7c4d
Abdulaziz Mohammed <azozmohd38@gmail.com>
2026-09-08 15:25:04 +0400

    Clean the multiplication loop condition

--- MultiplicationTable/MultiplicationTable.java
+++ MultiplicationTable/MultiplicationTable.java
@@ -5,7 +5,7 @@ public class MultiplicationTable {
        //Declare an int variable named base and assign it a value of 9
        final int base = 9;
        //Use a for loop that runs a counter variable from 1 to 10
-        for (int i = 1; i <= 10  ; i++){
+        for (int i = 1; i <= 10; i++){
          int result = base * i ;
          System.out.println(base + " x " + i + " = " + result);
         }
