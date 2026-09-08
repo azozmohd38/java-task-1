853a5b42bd4d80b9a285ac648f0072d57b34a435
Abdulaziz Mohammed <azozmohd38@gmail.com>
2026-09-08 15:24:26 +0400

    Track the number of even values

--- EvenNumberAnalyzer/EvenNumberAnalyzer.java
+++ EvenNumberAnalyzer/EvenNumberAnalyzer.java
@@ -11,7 +11,7 @@ public class EvenNumberAnalyzer {
         //Declare an int variable named limit and set it to 20
         final int limit = 20;
         //Declare an int variable named count and initialise it to 0
-        int count = 0;
+        int count = 0; // tracks the even numbers found
         //Declare an int variable named sum and initialise it to 0
         int sum = 0;
         // loop to iterate from 1 to limit
